ec9e35aee9493924467b35c46d9638df7c607135
ductruonghoc <92839932+ductruonghoc@users.noreply.github.com>
2022-06-19 21:10:20 +0700

    Update regSys.py

--- regSys.py
+++ regSys.py
@@ -7,20 +7,54 @@ BUFSIZ = 1024
 def regist_process(client):
     client.send(bytes("Enter new username", "utf8"))
     username = client.recv(BUFSIZ).decode("utf8")
-    # file hasnt created yet
+    #file hasnt created yet
     if path.isfile('account.json') is False:
         raise Exception("File not found", SERVER.close())
-    # open data file to read
+    #open data file to read
     fi = open('account.json')
-    # load json data to dict account
+    #load json data to dict account
     account = json.load(fi)
-    # checkduplicate username
-    if registUsernameCheck(account, username) is False:
+    #checkduplicate username	
+    if registUsernameCheck(account, username) is False:	
         client.send(bytes("Username has been created! Enter again", "utf8"))
         regist_process(client)
     else:
-        client.send(bytes("Username is valid! Now enter the password", "utf8"))
+        client.send(bytes("Username is valid!", "utf8"))
+            #check and create new password
+        password = registPassword(client)
+        #check and accept the pay method
+        pay = acceptPayMethod(client)
+        nAcc = {
+            "username": username,
+            "password": password,
+            "payID": pay
+        }
+        account['account'].append(nAcc)
+        fo = open('account.json', 'w')
+        json.dump(account, fo)
+        
+	
+def registUsernameCheck(account, username):	
+    for i in account['account']:
+        if username == i['username']:
+            return False
+    return True
 
+def registPassword(client):
+    client.send(bytes("Enter new password", "utf8"))
+    password = client.recv(BUFSIZ).decode("utf8")
+    if len(password) < 3:
+        client.send(bytes("Password not valid! Try again", "utf8"))
+        password = registPassword(client)
+    return password
+
+def acceptPayMethod(client):
+    client.send(bytes("Enter pay-id: ", "utf8"))
+    pay = client.recv(BUFSIZ).decode("utf8")
+    if pay.isdigit() is False or len(pay) != 10:
+        client.send(bytes("Card number not valid! Try again", "utf8"))
+        pay = acceptPayMethod(client)
+    return pay
 
 def login_process(client):
     client.send(bytes("Enter username", "utf8"))
@@ -40,14 +74,6 @@ def login_process(client):
         login_process(client)
     else:
         client.send(bytes("Access Granted", "utf8"))
-
-
-def registUsernameCheck(account, username):
-    for i in account['account']:
-        if username == i['username']:
-            return False
-    return True
-
 
 def loginUsernameCheck(account, username, password):
     for i in account['account']:
